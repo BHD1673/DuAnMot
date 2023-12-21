5a370911ccd1e7c4f6c1cd1565820ad7a9bcb36b
BHD1673 <banhaiduong167@gmail.com>
2023-12-21 20:55:42 +0700

    Tạo khung xương cho admin

--- admin.php
+++ admin.php
@@ -1,26 +1,143 @@
+
+<?php 
+//Hàm xử lý hành động cho admin
+function xuLyHanhDong($hanhDong) {
+    switch ($hanhDong) {
+        case 'taoloaisanpham':
+            taoLoaiSanPham();
+            break;
+        case 'hienthiloaisanpham':
+            hienThiLoaiSanPham();
+            break;
+        case 'hienthichitietloaisanpham':
+            hienThiChiTietLoaiSanPham();
+            break;
+        case 'taotaikhoan':
+            taoTaiKhoan();
+            break;
+        case 'hienthichitiettaikhoan':
+            hienThiChiTietTaiKhoan();
+            break;
+        case 'hienthitaikhoan':
+            hienThiTaiKhoan();
+            break;
+        case 'taodonhangonlan':
+            taoDonHangOnLan();
+            break;
+        case 'taodonhangonline':
+            taoDonHangOnline();
+            break;
+        case 'hienthichitietdonhang':
+            hienThiChiTietDonHang();
+            break;
+        case 'hienthidonhang':
+            hienThiDonHang();
+            break;
+        case 'hienthitrangchuadmin':
+            hienThiTrangChuAdmin();
+            break;
+        case 'thongke':
+            thongKe();
+            break;
+        case 'gioithieu':
+            gioiThieu();
+            break;
+        case 'baiviet':
+            baiViet();
+            break;
+        // case 'dangxuat':
+        //     dangXuatNguoiDung();
+        //     break;
+        // case 'dangnhap':
+        //     dangNhapNguoiDung();
+        //     break;
+        // case 'dangky':
+        //     dangKyNguoiDung();
+        //     break;
+        // case 'quenmatkhau':
+        //     quenMatKhau();
+        //     break;
+        default:
+            hienThiTrangChuAdmin();
+            break;
+    }
+}
+
+
+
+function taoLoaiSanPham() {
+    include "admin/view/LoaiSanPham/LoaiSanPham.Create.php";
+}
+
+function hienThiLoaiSanPham() {
+    include "admin/view/LoaiSanPham/LoaiSanPham.All.php";
+}
+
+function hienThiChiTietLoaiSanPham() {
+    include "admin/view/LoaiSanPham/LoaiSanPham.Custom.php";
+}
+
+//Phần tạo tài khoản sẽ bao gồm cả phần tạo thêm địa chỉ trong trường 
+//hợp muốn tạo đơn ship cho khách hàng. Hoặc tùy. Chúa chịu đoạn này đang
+//hơi lú
+function taoTaiKhoan() {
+    include "admin/view/TaiKhoan/TaiKhoan.Create.php";
+}
+
+//Hàm này sẽ bao gồm cả phần cập nhật, vì vẫn có khả năng có người vào chỉ muốn xem thông tin trang
+function hienThiChiTietTaiKhoan() {
+    include "admin/view/TaiKhoan/TaiKhoan.Custom.php";
+}
+
+function hienThiTaiKhoan() {
+    include "admin/view/TaiKhoan/TaiKhoan.All.php";
+}
+
+//Onlan = Đặt hàng trực tiếp tại của hàng
+//Online = Đặt hàng cho khách làm online
+function taoDonHangOnLan() {
+    include "admin/view/DonHang/DonHang.Create.OnLan.php";
+}
+
+function taoDonHangOnline() {
+    include "admin/view/DonHang/DonHang.Create.OnLine.php";
+}
+
+function hienThiChiTietDonHang() {
+    include "admin/view/DonHang/DonHang.Custom.php";
+}
+
+function hienThiDonHang() {
+    include "admin/view/DonHang/DonHang.All.php";
+}
+
+function hienThiTrangChuAdmin() {
+    include "admin/view/index.php";
+}
+
+function thongKe() {
+    include "admin/view/ThongKe/ThongKe.php";
+}
+
+//Cái của nợ bên dưới nên cho hẳn một file khác
+?>
+
+
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
 <?php include "admin/view/head.php"; ?>
 <body>
-    <!-- <div class="preloader">
-        <div class="lds-ripple">
-            <div class="lds-pos"></div>
-            <div class="lds-pos"></div>
-        </div>
-    </div> -->
     <div id="main-wrapper" data-sidebartype="full">
         <?php 
         include "admin/view/topbar.php";
         include "admin/view/leftSidebar.php";
 
-        include "admin/view/mainPage.php";
-
-        // if (isset($_GET['act'])) {
-        //     $hanhDong = $_GET['act'];
-        //     xuLyHanhDong($hanhDong);
-        // } else {
-        //     hienThiTrangChu();
-        // }
+        if (isset($_GET['act'])) {
+            $hanhDong = $_GET['act'];
+            xuLyHanhDong($hanhDong);
+        } else {
+            hienThiTrangChuAdmin();
+        }
 
         ?>
     </div>
@@ -28,3 +145,4 @@
     <div class="flotTip" style="display: none; position: absolute;"></div>
 </body>
 </html>
+
--- admin/view/hidden.php
+++ admin/view/hidden.php
@@ -1,25 +1,25 @@
 <div>
-        <script src="assets/admin/libs/jquery/assets/admin/dist/jquery.min.js"></script>
-        <!-- Bootstrap tether Core JavaScript -->
-        <script src="assets/admin/libs/popper.js/assets/admin/dist/umd/popper.min.js"></script>
-        <script src="assets/admin/libs/bootstrap/assets/admin/dist/js/bootstrap.min.js"></script>
-        <script src="assets/admin/libs/perfect-scrollbar/assets/admin/dist/perfect-scrollbar.jquery.min.js"></script>
-        <script src="assets/admin/extra-libs/sparkline/sparkline.js"></script>
-        <!--Wave Effects -->
-        <script src="assets/admin/dist/js/waves.js"></script>
-        <!--Menu sidebar -->
-        <script src="assets/admin/dist/js/sidebarmenu.js"></script>
-        <!--Custom JavaScript -->
-        <script src="assets/admin/dist/js/custom.min.js"></script>
-        <!--This page JavaScript -->
-        <!-- <script src="assets/admin/dist/js/pages/dashboards/dashboard1.js"></script> -->
-        <!-- Charts js Files -->
-        <script src="assets/admin/libs/flot/excanvas.js"></script>
-        <script src="assets/admin/libs/flot/jquery.flot.js"></script>
-        <script src="assets/admin/libs/flot/jquery.flot.pie.js"></script>
-        <script src="assets/admin/libs/flot/jquery.flot.time.js"></script>
-        <script src="assets/admin/libs/flot/jquery.flot.stack.js"></script>
-        <script src="assets/admin/libs/flot/jquery.flot.crosshair.js"></script>
-        <script src="assets/admin/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
-        <script src="assets/admin/dist/js/pages/chart/chart-page-init.js"></script>
-    </div>
+    <script src="admin/matrix-admin-master/assets/libs/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="admin/matrix-admin-master/assets/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="admin/matrix-admin-master/assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="admin/matrix-admin-master/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="admin/matrix-admin-master/assets/extra-libs/sparkline/sparkline.js"></script>
+    <!--Wave Effects -->
+    <script src="admin/matrix-admin-master/dist/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="admin/matrix-admin-master/dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="admin/matrix-admin-master/dist/js/custom.min.js"></script>
+    <!--This page JavaScript -->
+    <script src="admin/matrix-admin-master/dist/js/pages/dashboards/dashboard1.js"></script>
+    <!-- Charts js Files -->
+    <script src="admin/matrix-admin-master/assets/libs/flot/excanvas.js"></script>
+    <script src="admin/matrix-admin-master/assets/libs/flot/jquery.flot.js"></script>
+    <script src="admin/matrix-admin-master/assets/libs/flot/jquery.flot.pie.js"></script>
+    <script src="admin/matrix-admin-master/assets/libs/flot/jquery.flot.time.js"></script>
+    <script src="admin/matrix-admin-master/assets/libs/flot/jquery.flot.stack.js"></script>
+    <script src="admin/matrix-admin-master/assets/libs/flot/jquery.flot.crosshair.js"></script>
+    <script src="admin/matrix-admin-master/assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
+    <script src="admin/matrix-admin-master/dist/js/pages/chart/chart-page-init.js"></script>
+</div>
